a8a43c5cf87dedfb37db502211f819b34dabe7d4
pavan-pachava <ppavan2001@gmail.com>
2024-02-21 16:21:04 +0530

    added 3d map

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -7,40 +7,53 @@ const HomePage = () => {
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapContainerRef.current,
-            style: 'mapbox://styles/pxvan2204/clssus7pe00so01qu4dj99619',
+            style: 'mapbox://styles/pxvan2204/clsvo1csj005801qj2xja7t29',
             accessToken: 'pk.eyJ1IjoicHh2YW4yMjA0IiwiYSI6ImNsc2xsODk1cTBjNzIybG53cmVtZ2hjeWMifQ.HbtiE9XvtXZpNR2CF0YWoA'
         });
 
         map.on('load', () => {
-            map.addSource('route', {
-                'type': 'geojson',
-                'data': {
-                    'type': 'Feature',
-                    'properties': {},
-                    'geometry': {
-                        'type': 'LineString',
-                        'coordinates': [
-                            [78.348587, 17.447792], // replace with your first coordinate
-                            [78.351044, 17.445612]  // replace with your second coordinate
-                        ]
-                    }
-                }
-            });
 
-            map.addLayer({
-                'id': 'route',
-                'type': 'line',
-                'source': 'route',
-                'layout': {
-                    'line-join': 'round',
-                    'line-cap': 'round'
-                },
-                'paint': {
-                    'line-color': '#fff',
-                    'line-width': 8
-                }
+            const lines = [
+                [[78.348587, 17.447792], [78.351097, 17.445673]],
+                [[78.348587, 17.447792], [78.347332, 17.446451]],
+                // add more lines as needed
+            ];
+
+            lines.forEach((coordinates, index) => {
+                const id = 'line' + index;
+
+                map.addSource(id, {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'Feature',
+                        'properties': {},
+                        'geometry': {
+                            'type': 'LineString',
+                            'coordinates': coordinates
+                        }
+                    }
+                });
+
+                map.addLayer({
+                    'id': id,
+                    'type': 'line',
+                    'source': id,
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                        'line-color': 'black',
+                        'line-width': 5
+                    }
+                });
+
+                // Add markers to both ends of the line segment
+                new mapboxgl.Marker().setLngLat(coordinates[0]).addTo(map);
+                new mapboxgl.Marker().setLngLat(coordinates[1]).addTo(map);
             });
         });
+
 
         return () => map.remove();
     }, []);
@@ -48,6 +61,7 @@ const HomePage = () => {
     return (
         <div>
             <div ref={mapContainerRef} style={{ width: '100%', height: '800px' }} />
+            
         </div>
     );
 };
