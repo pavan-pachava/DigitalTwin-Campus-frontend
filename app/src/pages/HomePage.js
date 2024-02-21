829fd3297ab2098d34ed0d8caf074db44a983b26
pavan-pachava <ppavan2001@gmail.com>
2024-02-21 15:30:41 +0530

    added map and 1 network line

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = 'your_access_token_here'; // replace with your Mapbox access token
-
 const HomePage = () => {
     const mapContainerRef = useRef(null);
 
@@ -10,16 +8,46 @@ const HomePage = () => {
         const map = new mapboxgl.Map({
             container: mapContainerRef.current,
             style: 'mapbox://styles/pxvan2204/clssus7pe00so01qu4dj99619',
+            accessToken: 'pk.eyJ1IjoicHh2YW4yMjA0IiwiYSI6ImNsc2xsODk1cTBjNzIybG53cmVtZ2hjeWMifQ.HbtiE9XvtXZpNR2CF0YWoA'
         });
 
-        // Clean up on unmount
+        map.on('load', () => {
+            map.addSource('route', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': [
+                            [78.348587, 17.447792], // replace with your first coordinate
+                            [78.351044, 17.445612]  // replace with your second coordinate
+                        ]
+                    }
+                }
+            });
+
+            map.addLayer({
+                'id': 'route',
+                'type': 'line',
+                'source': 'route',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-color': '#fff',
+                    'line-width': 8
+                }
+            });
+        });
+
         return () => map.remove();
-    }, []); // Empty dependency array means this effect runs once on mount
+    }, []);
 
     return (
         <div>
-            <h1>Home Page</h1>
-            <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />
+            <div ref={mapContainerRef} style={{ width: '100%', height: '800px' }} />
         </div>
     );
 };
